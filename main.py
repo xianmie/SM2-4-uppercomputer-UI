f9e2d4da955cee499a5167edd835584a174a5687
xianmie <echo_xuanyoung@outlook.com>
2023-02-21 12:00:53 +0800

    complete 1.0

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton,QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox, QStatusBar
 from PyQt5.QtCore import QTimer
 import Menu
 import WriteKey
@@ -17,13 +17,108 @@ class WriteKey(WriteKey.Ui_MainWindow, QMainWindow):
     def __init__(self):
         super(WriteKey, self).__init__()
         self.setupUi(self)  # 初始化
-        self.pushButton_return.clicked.connect(self.close)
+        self.ser = serial.Serial()
+        self.ser.setRTS(False)
+        self.ser.setDTR(False)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.receive_data)
+        self.uart_open()
+        self.pushButton_return.clicked.connect(self.back)
+        self.pushButton_start.clicked.connect(self.send_data)
+
+    def uart_open(self):
+        self.ser.port = "COM5"
+        self.ser.baudrate = 115200
+        self.ser.bytesize = 8
+        self.ser.stopbits = 1
+        self.ser.parity = 'N'
+        try:
+            self.ser.open()
+        except:
+            QMessageBox.warning(self, "错误", "无法打开此串口！")
+            return
+
+        if self.ser.isOpen():
+            # 打开串口接收定时器，周期为10ms
+            self.timer.start(5)
+
+    def send_data(self):
+        if self.ser.isOpen():
+            send_data = self.plainTextEdit.toPlainText()
+            send_data += '\r\n'
+            send_data = send_data.encode('UTF-8')
+            self.ser.write(send_data)
+
+    def receive_data(self):
+        if self.ser.isOpen():
+            try:
+                num = self.ser.inWaiting()
+            except:
+                QMessageBox.warning(self, "错误", "接收数据错误")
+            if num > 0:
+                data = self.ser.read(num)
+                self.statusbar.showMessage(data.decode('UTF-8'))
+
+    def back(self):
+        self.ser.close()
+        self.close()
 
 class SM2(SM2.Ui_MainWindow, QMainWindow):
     def __init__(self):
         super(SM2, self).__init__()
         self.setupUi(self)  # 初始化
-        self.pushButton_return.clicked.connect(self.close)
+        self.ser = serial.Serial()
+        self.ser.setRTS(False)
+        self.ser.setDTR(False)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.receive_data)
+        self.uart_open()
+        self.pushButton_return.clicked.connect(self.back)
+        self.pushButton_start.clicked.connect(self.send_data)
+
+    def uart_open(self):
+        self.ser.port = "COM5"
+        self.ser.baudrate = 115200
+        self.ser.bytesize = 8
+        self.ser.stopbits = 1
+        self.ser.parity = 'N'
+        try:
+            self.ser.open()
+        except:
+            QMessageBox.warning(self, "错误", "无法打开此串口！")
+            return
+
+        if self.ser.isOpen():
+            # 打开串口接收定时器，周期为10ms
+            self.timer.start(5)
+
+    def send_data(self):
+        if self.ser.isOpen():
+            send_data = self.plainTextEdit.toPlainText()
+            send_data += '\r\n'
+            send_data = send_data.encode('UTF-8')
+            self.ser.write(send_data)
+
+    def receive_data(self):
+        if self.ser.isOpen():
+            try:
+                num = self.ser.inWaiting()
+            except:
+                QMessageBox.warning(self, "错误", "接收数据错误")
+            if num > 0:
+                # 获取到text光标
+                textCursor = self.textBrowser.textCursor()
+                # 滚动到底部
+                textCursor.movePosition(textCursor.End)
+                # 设置光标到text中去
+                self.textBrowser.setTextCursor(textCursor)
+                data = self.ser.read(num)
+                self.textBrowser.insertPlainText(data.decode('utf-8'))
+                self.textBrowser.moveCursor(self.textBrowser.textCursor().End)  # 文本框显示到底部
+
+    def back(self):
+        self.ser.close()
+        self.close()
 
 class SM4En(SM4En.Ui_MainWindow, QMainWindow):
     def __init__(self):
@@ -117,6 +212,11 @@ class SM4De(SM4De.Ui_MainWindow, QMainWindow):
             send_data += '\r\n'
             send_data = send_data.encode('UTF-8')
             self.ser.write(send_data)
+            sleep(0.1)
+            send_data = self.plainTextEdit_handle.toPlainText()
+            send_data += '\r\n'
+            send_data = send_data.encode('UTF-8')
+            self.ser.write(send_data)
 
     def receive_data(self):
         if self.ser.isOpen():
@@ -132,7 +232,7 @@ class SM4De(SM4De.Ui_MainWindow, QMainWindow):
             self.textBrowser.setTextCursor(textCursor)
             data = self.ser.read(num)
             self.textBrowser.insertPlainText(data.decode('utf-8'))
-            self.textBrowser.moveCursor(self.textBrowser.textCursor().End)  # 文本框显示到底部
+           # self.textBrowser.moveCursor(self.textBrowser.textCursor().End)  # 文本框显示到底部
 
     def back(self):
         self.ser.close()
@@ -166,7 +266,7 @@ class Menu(Menu.Ui_Menu, QMainWindow):
     def sm2(self):
         if not self.ser.isOpen():
             self.uart_open()
-        send_data = '1'
+        send_data = '0'
         send_data+='\r\n'
         send_data = send_data.encode('UTF-8')
         self.ser.write(send_data)
